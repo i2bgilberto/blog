250ebf7ee7890fd7134f45da2dc074b03e44372c
Ramon Alcala <i2bgilberto@gmail.com>
2024-04-09 21:22:36 -0500

    Many changes starting design blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://blog.i2b.dev",
-  integrations: [preact()]
+  integrations: [preact(), icon()],
 });
