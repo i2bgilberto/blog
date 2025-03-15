ff321850fdbd5fc472fdf1d521a6fa913c2de95d
Ramon Alcala <43355033+i2bgilberto@users.noreply.github.com>
2025-03-15 13:09:17 -0500

    Update change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import icon from "astro-icon";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://blog.i2b.dev",
   integrations: [preact(), icon()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
